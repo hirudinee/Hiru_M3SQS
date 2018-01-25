677789289eed44e79e62356ded1ec14816b2ede8
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-25 14:40:02 +0530

    issue 34

---
+++ Hiru_M3SQS/lambda.js
@@ -0,0 +1,27 @@
+let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
+exports.handler = function (event, context, callback) {
+	sqs.sendMessage({
+		MessageBody: 'test',
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/Hiru_Test1201',
+		DelaySeconds: '0',
+		MessageAttributes: {
+			"key01": {
+				"DataType": "String",
+				"StringValue": "test"
+			},
+			"key02": {
+				"DataType": "Number",
+				"StringValue": "001"
+			}
+		}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
+
+
+	callback(null, 'Successfully executed');
+}
